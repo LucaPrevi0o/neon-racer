970cf475dd84ce92280dc17113c2e7105a8c62a1
Luca Previati <luca@previati.com>
2026-08-03 09:38:10 +0200

    Route the Home menu to the track editor

--- src/app/main_menu_flow.cpp
+++ src/app/main_menu_flow.cpp
@@ -1,26 +1,26 @@
 #include "main_menu_flow.hpp"
 
 MainMenuFlow::MainMenuFlow()
-    : selectedChoice_(MainMenuChoice::CreateNewTrack), pendingAction_(MainMenuAction::None) {
+    : selectedChoice_(MainMenuChoice::OpenTrackEditor), pendingAction_(MainMenuAction::None) {
 }
 
 MainMenuChoice MainMenuFlow::SelectedChoice() const { return selectedChoice_; }
 
 void MainMenuFlow::SelectPrevious() {
     selectedChoice_ = selectedChoice_ == MainMenuChoice::PlayCompleteTrack
-        ? MainMenuChoice::CreateNewTrack : MainMenuChoice::PlayCompleteTrack;
+        ? MainMenuChoice::OpenTrackEditor : MainMenuChoice::PlayCompleteTrack;
 }
 
 void MainMenuFlow::SelectNext() {
     selectedChoice_ = selectedChoice_ == MainMenuChoice::PlayCompleteTrack
-        ? MainMenuChoice::CreateNewTrack : MainMenuChoice::PlayCompleteTrack;
+        ? MainMenuChoice::OpenTrackEditor : MainMenuChoice::PlayCompleteTrack;
 }
 
 void MainMenuFlow::Select(MainMenuChoice choice) { selectedChoice_ = choice; }
 
 void MainMenuFlow::ActivateSelectedChoice() {
     pendingAction_ = selectedChoice_ == MainMenuChoice::PlayCompleteTrack
-        ? MainMenuAction::OpenPlayableLibrary : MainMenuAction::BeginNewTrack;
+        ? MainMenuAction::OpenPlayableLibrary : MainMenuAction::OpenTrackEditor;
 }
 
 MainMenuAction MainMenuFlow::ConsumeAction() {
